6a6fcb8a57d0e0a71ecbb30830a3f429aed7251d
João Paulo Penney <137862398+joaopaulopenney@users.noreply.github.com>
2024-02-03 23:52:48 -0300

    update desafio01

---
+++ desafio01/desafio01-pratica/src/App.jsx
@@ -0,0 +1,44 @@
+import { login } from './utils';
+import './index.css';
+import { useState } from 'react';
+
+// Instruções:
+// * Você tem um formulário de login INCOMPLETO
+// * Não é permitido adicionar novos elementos HTML
+// * Não é permitido usar refs
+//
+// Tarefas:
+// todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
+// todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
+// todo - Desabilite o botão de Login equanto você está executando o login.
+// todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
+// todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [res, setRes] = useState();
+  const [err, setErr] = useState();
+
+  return (
+    <div className='wrapper'>
+      <div className='login-form'>
+        <h1>Login Form 🐞</h1>
+        {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
+        { err && <div className='errorMessage'>{err}</div>}
+        <div className='row'>
+          <label htmlFor={'email'}>Email</label>
+          <input id={'email'} type={'email'} autoComplete='off' onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div className='row'>
+          <label htmlFor={'password'}>Password</label>
+          <input id={'password'} type={'password'} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+
+        <div className='button'>
+          <button onClick={() => login(email, password).then((res) => {console.log(res)}).catch((err) => setErr(err.message))}>Login</button>
+        </div>
+      </div>
+    </div>
+  );
+}
