82b8d43474fdb70e40a13c4bf6644a9f0b773110
João Paulo Penney <137862398+joaopaulopenney@users.noreply.github.com>
2024-02-25 07:56:44 -0300

    update desafio05

--- desafio05/desafio05-pratica/src/App.jsx
+++ desafio05/desafio05-pratica/src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+
+import axios from "axios";
+
+/*
+Consuma a API e liste todos os pokemons da consulta do seguinte endpoint. 
+https://pokeapi.co/api/v2/pokemon
+
+Você deve exibir, de cada pokémon:
+- imagem
+- nome
+- experiência
+
+Você pode acessar as informações de cada pokemón individualmente em:
+https://pokeapi.co/api/v2/pokemon/:id
+
+
+DICA:
+imagem => sprites.front_default
+experiência => base_experience
+
+EXTRA: se puder ordene por nome.
+*/
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokes, setPokes] = useState([]);
+
+  const getPokemon = () => {
+    for ( let i = 1 ; i <= 25 ; i++ ) {
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`).then((res) => setPokes((prev) => [...prev, res])).catch((err) => console.log(err));
+    }
+}
+
+  useEffect(() => {
+    
+    getPokemon();
+
+  }, []);
+
+  console.log(pokes);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h3>desafio fernandev</h3>
+      <h1>consumir api pokémon</h1>
+      <div className='pokemons' style={{ display:"flex", flexDirection:"column" }}>
+        {
+          pokes.map((pokemon, key) => (
+            <div className='pokemon' key={key}><img src={pokemon.data.sprites.front_default} alt="Pokemon" /> {pokemon.data.name} EXP:{pokemon.data.base_experience}</div>
+          ))
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
