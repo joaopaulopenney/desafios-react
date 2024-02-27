0947a124fda24aba024de680b0dc13f14649d96c
João Paulo Penney <137862398+joaopaulopenney@users.noreply.github.com>
2024-02-27 08:28:16 -0300

    update desafio04

--- desafio04/desafio04-pratica/src/App.jsx
+++ desafio04/desafio04-pratica/src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './styles.scss';
+
+/*
+  DESAFIO TÉCNICO - JOGO DA VELHA - por fernandev
+
+  * descrição
+    desenvolva um jogo da velha (tic tac toe) funcional.
+    use qualquer técnica de estilização preferida: css modules, sass, styled.
+
+  * tasks
+    ? - crie um board de 3x3
+    ? - dois jogadores
+    ? - ao clicar em um quadrado, preencher com a jogada
+    ? - avisar quando o jogo finalizar, caso dê velha avise também
+    ? - fazer um risco na sequência vencedora, caso houver
+*/
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [player, setPlayer] = useState(true);
+  const [games, setGames] = useState([]);
+
+  const game = (e) => {
+    if (player) {
+      e.target.innerText = "X";
+      setPlayer(false);
+    } else {
+      e.target.innerText = "O";
+      setPlayer(true);
+    }
+
+    
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h3>desafio fernandev</h3>
+      <h1>jogo da velha</h1>
+      <div className='board-game' onClick={(e) => game(e)}>
+        <div className='item' id='1'></div>
+        <div className='item' id='2'></div>
+        <div className='item' id='3'></div>
+        <div className='item' id='4'></div>
+        <div className='item' id='5'></div>
+        <div className='item' id='6'></div>
+        <div className='item' id='7'></div>
+        <div className='item' id='8'></div>
+        <div className='item' id='9'></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
